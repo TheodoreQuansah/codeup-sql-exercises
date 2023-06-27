920e5d9c66a199a2b9cd7417b95372d6c3f28550
TheodoreQuansah <theodore.s.quansah@gmail.com>
2023-06-27 10:53:23 -0500

    db_tables_exercises2.sql

--- db_tables_exercises2.sql
+++ db_tables_exercises2.sql
@@ -8,19 +8,19 @@ USE albums_db;
 -- The name of the albums
 
 -- What do you think the sales column represents?
--- the number of albums sold (float)
+-- the number of albums sold
 
 -- Find the name of all albums by Pink Floyd.
--- The Dark Side of the Moon, The Wall. SELECT artist, name FROM albums WHERE artist = 'Pink Floyd'; 
+ SELECT artist, name FROM albums WHERE artist = 'Pink Floyd'. -- The Dark Side of the Moon, The Wall.; 
 
 -- What is the year Sgt. Pepper's Lonely Hearts Club Band was released?
--- 1967. SELECT release_date FROM albums WHERE name = 'Sgt. Pepper's Lonely Hearts Club Band was released'
+SELECT release_date FROM albums WHERE name = 'Sgt. Pepper's Lonely Hearts Club Band was released'. -- 1967.
 
 -- What is the genre for the album Nevermind?
--- Grunge, Alternative rock. SELECT artist, name, genre FROM albums WHERE name = 'Nevermind';
+SELECT artist, name, genre FROM albums WHERE name = 'Nevermind';. -- Grunge, Alternative rock. 
 
 -- Which albums were released in the 1990s?
--- SELECT name, release_date FROM albums WHERE release_date BETWEEN 1990 AND 1999;
+SELECT name, release_date FROM albums WHERE release_date BETWEEN 1990 AND 1999;
 
 -- Which albums had less than 20 million certified sales? Rename this column as low_selling_albums.
--- SELECT name, sales AS low_selling_albums FROM albums WHERE sales < 20;
+SELECT name, sales AS low_selling_albums FROM albums WHERE sales < 20;
