cacebf1dae133a8e7274fc1676cb2cafce0c1762
TheodoreQuansah <theodore.s.quansah@gmail.com>
2023-06-27 15:23:14 -0500

    order_by_exercises.sql

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -57,32 +57,32 @@ FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
 Find all employees with first names 'Irena', 'Vidya', or 'Maya', and order your results returned by first name. In your comments, answer: What was the first and last name in the first row of the results? What was the first and last name of the last person in the table?
 SELECT emp_no, first_name, last_name 
 FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya')
-ORDER BY first_name;
+ORDER BY first_name ASC;
 -- Irena Reutenauer.
 -- Vidya Simmen.
 
 Find all employees with first names 'Irena', 'Vidya', or 'Maya', and order your results returned by first name and then last name. In your comments, answer: What was the first and last name in the first row of the results? What was the first and last name of the last person in the table?
 SELECT first_name, last_name
 FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya')
-ORDER BY first_name, last_name;
--- Irena Acton, Vidya Zweizig
+ORDER BY first_name ASC, last_name ASC;
+-- Irena Acton, Vidya Zweizig.
 
 Find all employees with first names 'Irena', 'Vidya', or 'Maya', and order your results returned by last name and then first name. In your comments, answer: What was the first and last name in the first row of the results? What was the first and last name of the last person in the table?
 SELECT first_name, last_name
 FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya')
-ORDER BY last_name, first_name;
+ORDER BY last_name ASC, first_name ASC; 
 -- Irena Acton, Maya Zyda.
 
 Write a query to to find all employees whose last name starts and ends with 'E'. Sort the results by their employee number. Enter a comment with the number of employees returned, the first employee number and their first and last name, and the last employee number with their first and last name.
 SELECT DISTINCT *
 FROM employees WHERE last_name LIKE 'e%e'
-ORDER BY emp_no;
+ORDER BY emp_no ASC;
 -- 899, 10021-Erde Ramzi. 499648-Erde Tadahiro.
 
 Write a query to to find all employees whose last name starts and ends with 'E'. Sort the results by their hire date, so that the newest employees are listed first. Enter a comment with the number of employees returned, the name of the newest employee, and the name of the oldest employee.
 SELECT DISTINCT *
 FROM employees WHERE last_name LIKE 'e%e'
-ORDER BY hire_date desc;
+ORDER BY hire_date DESC;
 -- 899, Teiji Eldridge, Sergi Erde.
 
 Find all employees hired in the 90s and born on Christmas. Sort the results so that the oldest employee who was hired last is the first result. Enter a comment with the number of employees returned, the name of the oldest employee who was hired last, and the name of the youngest employee who was hired first.
