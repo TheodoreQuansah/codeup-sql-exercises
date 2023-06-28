d9423b85ff2194aa0615a3f99ea734fc56966c27
TheodoreQuansah <theodore.s.quansah@gmail.com>
2023-06-28 16:54:01 -0500

    functions_exercises.sql

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -115,9 +115,6 @@ Find the smallest and largest current salary from the salaries table.
 SELECT MIN(salary), MAX(salary) from salaries;
 
 Use your knowledge of built in SQL functions to generate a username for all of the employees. A username should be all lowercase, and consist of the first character of the employees first name, the first 4 characters of the employees last name, an underscore, the month the employee was born, and the last two digits of the year that they were born. Below is an example of what the first 10 rows will look like:
-SELECT first_name, last_name, birth_date, 
-CONCAT (SUBSTR(LOWER(first_name), 1, 1), 
-SUBSTR(LOWER(last_name), 1, 4)
-,'_', SUBSTR(REPLACE(birth_date, '-', ''), 5,2), 
-SUBSTR(REPLACE(birth_date, '-', ''), 3,2)) 
-AS username FROM employees LIMIT 10;
+SELECT LOWER(CONCAT(SUBSTR(first_name, 1, 1), SUBSTR(last_name, 1, 4), '_', SUBSTR(birth_date, 6, 2), SUBSTR(birth_date, 3, 2))) AS user_name
+FROM employees 
+GROUP BY user_name
