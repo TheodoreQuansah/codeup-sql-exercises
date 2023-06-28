c643bf66763cbf007c56af433613659bb6bd134d
TheodoreQuansah <theodore.s.quansah@gmail.com>
2023-06-28 13:56:36 -0500

    functions_exercises.sql

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -95,23 +95,24 @@ ORDER BY birth_date ASC, hire_date DESC;
 -- FUNCTIONS EXERCISE
 
 Write a query to to find all employees whose last name starts and ends with 'E'. Use concat() to combine their first and last name together as a single column named full_name.
-SELECT *, CONCAT (first_name,' ', last_name) AS full_name FROM employees
+SELECT CONCAT (first_name,' ', last_name) AS full_name FROM employees
 WHERE last_name LIKE 'e%e';
 
 Convert the names produced in your last query to all uppercase.
-SELECT *, CONCAT (UPPER(first_name),' ', UPPER(last_name)) AS full_name FROM employees
+SELECT CONCAT (UPPER(first_name),' ', UPPER(last_name)) AS full_name FROM employees
 WHERE last_name LIKE 'e%e';
 
 Use a function to determine how many results were returned from your previous query.
-SELECT COUNT(last_name LIKE 'e%e' ) FROM employees;
+SELECT COUNT(*) AS result_count
+FROM employees
+WHERE last_name LIKE 'e%e'
 
 Find all employees hired in the 90s and born on Christmas. Use datediff() function to find how many days they have been working at the company (Hint: You will also need to use NOW() or CURDATE()),
 SELECT *, DATEDIFF(CURDATE(), hire_date) AS daysworked
 FROM employees WHERE hire_date LIKE '199%' AND birth_date LIKE '%12-25';
 
 Find the smallest and largest current salary from the salaries table.
-SELECT MIN(salary) from salaries;
-SELECT MAX(salary) from salaries;
+SELECT MIN(salary), MAX(salary) from salaries;
 
 Use your knowledge of built in SQL functions to generate a username for all of the employees. A username should be all lowercase, and consist of the first character of the employees first name, the first 4 characters of the employees last name, an underscore, the month the employee was born, and the last two digits of the year that they were born. Below is an example of what the first 10 rows will look like:
 SELECT first_name, last_name, birth_date, 
