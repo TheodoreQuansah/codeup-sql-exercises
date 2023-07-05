8971d0f7e9d0399d71b655ef5f4c89ca087a9f96
TheodoreQuansah <theodore.s.quansah@gmail.com>
2023-07-05 10:43:10 -0500

    group_by_exercises.sql

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,51 +1,78 @@
-In your script, use DISTINCT to find the unique titles in the titles table. How many unique titles have there ever been? Answer that in a comment in your SQL file.
-SELECT DISTINCT title
-FROM titles
+-- In your script, use DISTINCT to find the unique titles in the titles table. How many unique titles have there ever been? Answer that in a comment in your SQL file.
+SELECT DISTINCT
+    title
+FROM
+    titles
 GROUP BY title;
 -- 7
 
-Write a query to to find a list of all unique last names of all employees that start and end with 'E' using GROUP BY
-SELECT DISTINCT last_name
-FROM employees
-WHERE last_name LIKE 'e%e'
+SELECT DISTINCT
+    last_name
+FROM
+    employees
+WHERE
+    last_name LIKE 'e%e'
 GROUP BY last_name;
 
-Write a query to to find all unique combinations of first and last names of all employees whose last names start and end with 'E'.
-SELECT DISTINCT first_name, last_name 
-FROM employees
-GROUP BY first_name,last_name
+-- Write a query to to find all unique combinations of first and last names of all employees whose last names start and end with 'E'.
+SELECT DISTINCT
+    first_name, last_name
+FROM
+    employees
+GROUP BY first_name , last_name
 HAVING last_name LIKE 'e%e';
 
-Write a query to find the unique last names with a 'q' but not 'qu'. Include those names in a comment in your sql code.
-SELECT last_name
-FROM employees
-WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'
+-- Write a query to find the unique last names with a 'q' but not 'qu'. Include those names in a comment in your sql code.
+SELECT 
+    last_name
+FROM
+    employees
+WHERE
+    last_name LIKE '%q%'
+        AND last_name NOT LIKE '%qu%'
 GROUP BY last_name;
 -- Chleq, Lindqvist, Qiwen
 
-Add a COUNT() to your results (the query above) to find the number of employees with the same last name.
-SELECT COUNT(last_name)
-FROM employees
-WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'
+SELECT 
+    COUNT(last_name)
+FROM
+    employees
+WHERE
+    last_name LIKE '%q%'
+        AND last_name NOT LIKE '%qu%'
 GROUP BY last_name;
 
-Find all employees with first names 'Irena', 'Vidya', or 'Maya'. Use COUNT(*) and GROUP BY to find the number of employees for each gender with those names.
-SELECT COUNT(*) 
-FROM employees 
-WHERE first_name 
-IN ('Irena', 'Vidya', 'Maya') 
+-- Find all employees with first names 'Irena', 'Vidya', or 'Maya'. Use COUNT(*) and GROUP BY to find the number of employees for each gender with those names.
+SELECT 
+    COUNT(*)
+FROM
+    employees
+WHERE
+    first_name IN ('Irena' , 'Vidya', 'Maya')
 GROUP BY gender;
 
-Using your query that generates a username for all of the employees, generate a count employees for each unique username.
-SELECT LOWER(CONCAT(SUBSTR(first_name, 1, 1), SUBSTR(last_name, 1, 4), '_', SUBSTR(birth_date, 6, 2), SUBSTR(birth_date, 3, 2))) AS user_name, 
-COUNT(*) AS number_usernames 
-FROM employees 
-GROUP BY user_name
+-- Using your query that generates a username for all of the employees, generate a count employees for each unique username.
+SELECT 
+    LOWER(CONCAT(SUBSTR(first_name, 1, 1),
+                    SUBSTR(last_name, 1, 4),
+                    '_',
+                    SUBSTR(birth_date, 6, 2),
+                    SUBSTR(birth_date, 3, 2))) AS user_name,
+    COUNT(*) AS number_usernames
+FROM
+    employees
+GROUP BY user_name;
 
-From your previous query, are there any duplicate usernames? What is the higest number of times a username shows up? Bonus: How many duplicate usernames are there from your previous query?
-SELECT LOWER(CONCAT(SUBSTR(first_name, 1, 1), SUBSTR(last_name, 1, 4), '_', SUBSTR(birth_date, 6, 2), SUBSTR(birth_date, 3, 2))) AS user_name, 
-COUNT(*) AS number_usernames 
-FROM employees 
+-- From your previous query, are there any duplicate usernames? What is the higest number of times a username shows up? Bonus: How many duplicate usernames are there from your previous query?
+SELECT 
+    LOWER(CONCAT(SUBSTR(first_name, 1, 1),
+                    SUBSTR(last_name, 1, 4),
+                    '_',
+                    SUBSTR(birth_date, 6, 2),
+                    SUBSTR(birth_date, 3, 2))) AS user_name,
+    COUNT(*) AS number_usernames
+FROM
+    employees
 GROUP BY user_name
 HAVING number_usernames > 1
-ORDER BY COUNT(*) DESC
+ORDER BY COUNT(*) DESC;
